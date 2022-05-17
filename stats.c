bca82fc04432a73cb1f1ffbc9311c68bcea96189
Amit2509 <amitsrivastava2509@gmail.com>
2022-05-17 22:54:49 +0530

    Update stats.c
    updating compute_statistics definition

--- stats.c
+++ stats.c
@@ -5,6 +5,23 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    float sum = 0;
+    
+    for (int i=0; i<setlength; i++)
+    {
+        if(numberset[i] < s.min)
+        {
+            s.min = numberset[i];
+        }
+        else if(numberset[i] > s.max)
+        {
+            s.max = numberset[i];
+        }
+        sum += numberset[i];
+    }
+    s.average = sum/setlength;
+    
+    return s;
 }
 
 int emailAlertCallCount = 0;
